39190314ff9a4dcabf5ac1846465fdd935779c84
Bhaskar <bhaskarsrivastav24@gmail.com>
2022-11-22 17:31:59 +0530

    initial commit

---
+++ cocktailDB-react/src/pages/About.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+      sed do eiusmod tempor incididunt ut labore et dolore magna 
+      aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+       laboris nisi ut aliquip
+      </p>
+    </section>
+    
+      
+    
+  )
+}
+
+export default About
