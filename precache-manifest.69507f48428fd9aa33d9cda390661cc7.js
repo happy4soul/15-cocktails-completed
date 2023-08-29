d64bc248e7cd14328eebae02c2624d1ef618714c
Bhaskar <bhaskarsrivastav24@gmail.com>
2023-08-29 14:50:39 +0530

    Updates

---
+++ precache-manifest.69507f48428fd9aa33d9cda390661cc7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13f83fd2b5a332e6014521c88be64647",
+    "url": "/15-cocktails-completed/index.html"
+  },
+  {
+    "revision": "3a7f496e00ae2e88a79c",
+    "url": "/15-cocktails-completed/static/css/main.53dd988b.chunk.css"
+  },
+  {
+    "revision": "e1f6309a6f0d3e350015",
+    "url": "/15-cocktails-completed/static/js/2.72303b8e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/15-cocktails-completed/static/js/2.72303b8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a7f496e00ae2e88a79c",
+    "url": "/15-cocktails-completed/static/js/main.be381306.chunk.js"
+  },
+  {
+    "revision": "c31fc5113d6998ab7b6f",
+    "url": "/15-cocktails-completed/static/js/runtime-main.79732783.js"
+  },
+  {
+    "revision": "f3ad766494d1f9701717c1a860cd5b10",
+    "url": "/15-cocktails-completed/static/media/logo.f3ad7664.svg"
+  }
+]);
